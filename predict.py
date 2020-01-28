942963cef7e09b35e276609265d72996dfa257d5
Vita <vitalia@myrambler.ru>
2020-01-28 16:26:06 +0300

    + new model&pickle

--- predict.py
+++ predict.py
@@ -8,7 +8,22 @@ from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 from python_speech_features import mfcc
 import pickle
+import sys
 
+sys.path.append('pickles')
+
+
+class Config:
+    def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft=512, rate=16000):  # filtered out
+        self.mode = mode
+        self.nfilt = nfilt
+        self.nfeat = nfeat
+        self.rate = rate
+        self.nfft = nfft
+        # 0.1 sec, how much data computing while creating window
+        self.step = int(rate/10)
+        self.model_path = os.path.join('models', mode + '.model')
+        self.p_path = os.path.join('pickles', mode + '.p')
 
 
 def build_predictions(audio_dir):
@@ -23,49 +38,52 @@ def build_predictions(audio_dir):
         c = classes.index(label)
         y_prob = []
 
-        for i in range(0, wav.shape[0]-config.step, config.step):#cannot go further
+        for i in range(0, wav.shape[0]-config.step, config.step):  # cannot go further
             sample = wav[i:i+config.step]
-            x = mfcc(sample, rate, numcep = config.nfeat, nfilt = config.nfilt, nfft = config.nfft)
-            x = (x - config.min)/(config.max - config.min)#range
+            x = mfcc(sample, rate, numcep=config.nfeat,
+                     nfilt=config.nfilt, nfft=config.nfft)
+            x = (x - config.min)/(config.max - config.min)  # range
             if config.mode == 'conv':
                 x = x.reshape(1, x.shape[0], x.shape[1], 1)
-            elif config.mode=='time':
-                x = np.expand_dims(x, axis = 0) #expand to 1 sample
+            elif config.mode == 'time':
+                x = np.expand_dims(x, axis=0)  # expand to 1 sample
+            elif config.mode == 'convtime':
+                x = x.reshape(1, x.shape[0], x.shape[1], 1)
             y_hat = model.predict(x)
             y_prob.append(y_hat)
             y_pred.append(np.argmax(y_hat))
             y_true.append(c)
 
-        fn_prob[fn] = np.mean(y_prob, axis = 0).flatten() #or that would be crap
+        # or that would be crap
+        fn_prob[fn] = np.mean(y_prob, axis=0).flatten()
 
     return y_true, y_pred, fn_prob
 
-df = pd.read_csv('instruments200.csv')#can remove if no classification
-classes = list(np.unique(df.label))#all names of genres 
+
+# can remove if no classification, if we don't know true classes
+df = pd.read_csv('new_try.csv')
+classes = list(np.unique(df.label))  # all names of genres
 
 fn2class = dict(zip(df.fname, df.label))
-p_path = os.path.join('pickles', 'conv.p')
+p_path = os.path.join('pickles', 'convbig.p')
 
 with open(p_path, 'rb') as handle:
     config = pickle.load(handle)
 
-model = load_model(config.model_path)
+model = load_model('models/conv.model')
 
-y_true, y_pred, fn_prob = build_predictions('clean200')
-acc_score = accuracy_score(y_true = y_true, y_pred = y_pred)
+y_true, y_pred, fn_prob = build_predictions(
+    'valrandmusic')  # predictions for all in this dir
+acc_score = accuracy_score(y_true=y_true, y_pred=y_pred)
 
 y_probs = []
-for i,row in df.iterrows():
-    print(row.fname)
+for i, row in df.iterrows():
     y_prob = fn_prob[row.fname]
     y_probs.append(y_prob)
-    for c ,p in zip(classes, y_prob):
+    for c, p in zip(classes, y_prob):
         df.at[i, c] = p
 
-print(classes)
 y_pred = [classes[np.argmax(y)] for y in y_probs]
 df['y_pred'] = y_pred
 
-df.to_csv('predictions200.csv', index = False)
-
-
+df.to_csv('conv_results.csv', index=False)
