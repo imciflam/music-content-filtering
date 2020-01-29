409d0b80612cff7eed54d74a3079a2a4599074fd
Vita <vitalia@myrambler.ru>
2020-01-29 11:57:05 +0300

    + converting

--- track_preparation.py
+++ track_preparation.py
@@ -1,6 +1,36 @@
 import os
 import glob
 from pydub import AudioSegment
+import os
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from python_speech_features import mfcc, logfbank
+
+
+def envelope(y, rate, threshold):  # signal envelope
+    mask = []
+    y = pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10), min_periods=1,
+                       center=True).mean()  # rolling window over data
+    for mean in y_mean:
+        if mean > threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask
+
+
+def calc_fft(y, rate):  # magnitute + freq (fft is complex)
+    n = len(y)
+    # amonth of time passing between each sample
+    freq = np.fft.rfftfreq(n, d=1/rate)
+    Y = abs(np.fft.rfft(y)/n)  # fft is complex, division to balance
+    return(Y, freq)
+
 
 standart_dir = os.getcwd()
 audio_dir = os.path.join(os.getcwd(), 'current_track')
@@ -12,3 +42,43 @@ for audio in glob.glob(extension):
         standart_dir + "/converted_track/" + wav_filename, format='wav')
 
 os.chdir(standart_dir)
+
+dictionary = {'fname': [
+    wav_filename,
+]}
+
+df = pd.DataFrame(data=dictionary)
+df.set_index('fname', inplace=True)
+
+for f in df.index:
+    if isinstance(f, str):
+        rate, signal = wavfile.read(standart_dir + "/converted_track/"+f)
+        df.at[f, 'length'] = signal.shape[0]/rate
+
+df.reset_index(inplace=True)
+
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+
+wav_file = df.iloc[0, 0]  # condition -0 ind, 0 col
+signal, rate = librosa.load(
+    standart_dir + "/converted_track/"+wav_file, sr=44100)  # sampling rate
+mask = envelope(signal, rate, 0.0005)  # can toggle 0.0005
+signal = signal[mask]  # remove noise
+signals[wav_file] = signal
+fft[wav_file] = calc_fft(signal, rate)
+# filterbank from pyspeechfeat, show a sec of data. nfft - calculated, short time Fourier trans
+bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+fbank[wav_file] = bank
+# calculated after discete cosine transform
+mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+mfccs[wav_file] = mel
+
+for f in tqdm(df.fname):
+    signal, rate = librosa.load(
+        standart_dir + "/converted_track/" + f, sr=16000)  # screw highfreq
+    mask = envelope(signal, rate, 0.0005)  # clean up the junk
+    wavfile.write(filename=standart_dir + "/converted_track/" +
+                  f, rate=rate, data=signal[mask])
