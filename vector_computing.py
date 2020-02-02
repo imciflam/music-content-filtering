8768ad8a5a884c896ca430a82d940521fb94bf9e
Vita <vitalia@myrambler.ru>
2020-02-02 16:24:08 +0300

    switched to dictionary

--- vector_computing.py
+++ vector_computing.py
@@ -7,6 +7,7 @@ import temp_audio_removal as tar
 from tqdm import tqdm
 from scipy.spatial import distance
 import numpy as np
+import time
 
 mock_input = {'fname': ['x'],
               'Electronic': [0.379083067],
@@ -22,11 +23,15 @@ mock_input = {'fname': ['x'],
 mock_input_df = pd.DataFrame(data=mock_input)
 dataset_df = pd.read_csv('conv_results.csv')
 
-distance_for_tracks = []
+distance_for_tracks = {}
 for index in dataset_df.index:
-    distance_for_tracks.append(distance.cosine(
+    distance_for_tracks[dataset_df.iloc[index, 0]] = (distance.cosine(
         mock_input_df.iloc[0, 1:9], dataset_df.iloc[index, 1:9]))
 
-print(distance_for_tracks)
-closest_index = distance_for_tracks.index(min(distance_for_tracks))
-closest_track_name = dataset_df.iloc[closest_index, 0]
+
+sorted_distance_dictionary = sorted(
+    distance_for_tracks.items(), key=lambda x: x[1])
+top_five_items = take(5, sorted_distance_dictionary.items())
+print(top_five_items)
+# closest_index = distance_for_tracks.index(min(distance_for_tracks))
+# closest_track_name = dataset_df.iloc[closest_index, 0]
