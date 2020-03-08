e746aa51959cb6695089742ea33177dd744fd312
Vita <vitalia@myrambler.ru>
2020-03-08 20:12:23 +0300

    datasets

--- vector_computing.py
+++ vector_computing.py
@@ -10,7 +10,7 @@ import numpy as np
 import time
 
 mock_input_df = pd.read_csv("conv_results.csv")
-dataset_df = pd.read_csv('conv_results_for_dataset.csv')
+dataset_df = pd.read_csv('spotify_preview_dataset.csv')
 
 
 def cosine_distance_calculation():
