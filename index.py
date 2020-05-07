7c3c34576973f497e342ee142511624437b42acf
Vita <vitalia@myrambler.ru>
2020-05-07 20:19:19 +0300

    - old data

--- index.py
+++ index.py
@@ -89,7 +89,8 @@ def make_classification(stable_wav_filenames):
 # model preloading
 model = load_model('models/conv.model')
 model._make_predict_function() 
-p_path = os.path.join('pickles', 'convbig.p')
+# change to conv.p if need some speed
+p_path = os.path.join('pickles', 'conv.p')
 with open(p_path, 'rb') as handle:
     config = pickle.load(handle)
 
